663b070cfa8ac9e6d2a7f8905d046bce16a08f53
Pedrag1234 <pedrag1234@gmail.com>
2019-10-16 14:24:46 +0100

    User login screen

---
+++ flutter_app/lib/app_screens/user_login.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+
+class UserLogin extends StatefulWidget {
+  @override
+  _UserLoginState createState() => _UserLoginState();
+}
+
+class _UserLoginState extends State<UserLogin> {
+  TextEditingController emailEditingContrller = TextEditingController();
+  TextEditingController passwordEditingContrller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //resizeToAvoidBottomInset: false,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Container(
+            color: Colors.white,
+            padding: EdgeInsets.all(24),
+            child: Center(
+              child: Column(
+                children: <Widget>[
+
+                  SizedBox( //caixa do email
+                    height: 30,
+                  ),
+                  TextField(
+                    autofocus: false,
+                    obscureText: false,
+                    keyboardType: TextInputType.emailAddress,
+                    controller: emailEditingContrller,
+                    decoration: InputDecoration(
+                        labelText: "Email",
+                        hintText: "Email",
+                        labelStyle: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                        ),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(4)),
+                            borderSide: BorderSide(
+                                width: 1,
+                                color: Colors.green,
+                                style: BorderStyle.solid))),
+                  ),
+
+
+                  SizedBox( // caixa da Password
+                    height: 30,
+                  ),
+                  TextField(
+                    autofocus: false,
+                    obscureText: true,
+                    keyboardType: TextInputType.text,
+                    controller: passwordEditingContrller,
+                    decoration: InputDecoration(
+                        labelText: "Password",
+                        hintText: "Password",
+                        labelStyle: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                        ),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(4)),
+                            borderSide: BorderSide(
+                                width: 1,
+                                color: Colors.green,
+                                style: BorderStyle.solid))),
+                  ),
+
+
+                  SizedBox( // button de LOGIN
+                    height: 50,
+                  ),
+                  ButtonTheme(
+                    //elevation: 4,
+                    //color: Colors.green,
+                    minWidth: double.infinity,
+                    child: MaterialButton(
+                      onPressed: () => {},
+                      textColor: Colors.white,
+                      color: Colors.green,
+                      height: 50,
+                      child: Text("LOGIN"),
+                    ),
+                  ),
+
+
+                  SizedBox( // button de SUBSCIBE
+                    height: 20,
+                  ),
+                  ButtonTheme(
+                    minWidth: double.infinity,
+                    child: MaterialButton(
+                      onPressed: () => {},
+                      textColor: Colors.white,
+                      color: Colors.red,
+                      height: 50,
+                      child: Text("SUBSCRIBE"),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
