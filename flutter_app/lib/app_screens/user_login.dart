62a43a97d2c51a55b83c9fa6e52be4ca7f103964
Pedrag1234 <pedrag1234@gmail.com>
2019-10-23 18:01:52 +0100

    login and register working, but if error ocurres the app still loads next page

--- flutter_app/lib/app_screens/user_login.dart
+++ flutter_app/lib/app_screens/user_login.dart
@@ -3,6 +3,9 @@ import 'package:flutter_app/app_screens/homePage.dart';
 import 'package:flutter_app/app_screens/main_Menu.dart';
 import 'package:flutter_app/app_screens/user_register.dart';
 import 'user_profile.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+
 
 
 class UserLogin extends StatefulWidget {
@@ -10,7 +13,141 @@ class UserLogin extends StatefulWidget {
   _UserLoginState createState() => _UserLoginState();
 }
 
-class _UserLoginState extends State<UserLogin> {//TODO: Criar tipo de letra no inicio para nao repetir em cada Textfield
+enum FormType {
+  login,
+  register
+}
+
+class _UserLoginState extends State<UserLogin> {
+  //TODO: Criar tipo de letra no inicio para nao repetir em cada Textfield
+
+
+
+  final formKey = GlobalKey<FormState>();
+
+  String _email, _password, errorMsg;
+  FormType _formType = FormType.login;
+  bool _loading = false;
+
+  bool validateandsave(){
+    final form = formKey.currentState;
+      if(form.validate()){
+        form.save();
+        _loading = true;
+        return true;
+      }
+      else{
+        return false;
+      }
+  }
+
+  void validateandsubmit() async {
+    if(validateandsave()){
+      try{
+        if(_formType == FormType.login){
+          FirebaseUser user = (await FirebaseAuth.instance.signInWithEmailAndPassword(email: _email, password: _password)).user;
+          print("Signed in: ${user.uid}");
+        }
+        else{
+          FirebaseUser user = (await FirebaseAuth.instance.createUserWithEmailAndPassword(email: _email, password: _password)).user;
+          print("Registered in: ${user.uid}");
+        }
+      } catch (e){
+        switch(e.code){
+          case "ERROR_USER_NOT_FOUND":
+            {
+              errorMsg =
+                "There is no user with such entries. Please try again.";
+              _loading = false;
+
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      content: Container(
+                        child: Text(errorMsg),
+                      ),
+                    );
+                  });
+            }
+            break;
+
+          case "ERROR_WRONG_PASSWORD":
+            {
+              errorMsg = "Password doesn\'t match your email.";
+              _loading = false;
+
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      content: Container(
+                        child: Text(errorMsg),
+                      ),
+                    );
+                  });
+            }
+            break;
+
+          case "ERROR_EMAIL_ALREADY_IN_USE":
+            {
+
+              errorMsg = "This email is already in use.";
+              _loading = false;
+
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      content: Container(
+                        child: Text(errorMsg),
+                      ),
+                    );
+                  });
+            }
+            break;
+
+          case "ERROR_WEAK_PASSWORD":
+            {
+
+              errorMsg = "The password must be 6 characters long or more.";
+              _loading = false;
+
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      content: Container(
+                        child: Text(errorMsg),
+                      ),
+                    );
+                  });
+            }
+            break;
+
+          default:
+            {
+
+              errorMsg = "";
+
+            }
+        }
+      }
+    }
+  }
+
+  void movetoregister(){
+    setState(() {
+      _formType = FormType.register;
+    });
+  }
+
+  void movetologin(){
+    setState(() {
+      _formType = FormType.login;
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -18,19 +155,12 @@ class _UserLoginState extends State<UserLogin> {//TODO: Criar tipo de letra no i
     return Scaffold(
       //resizeToAvoidBottomInset: false,
       body: Center(
-        child: SingleChildScrollView(
-          child: Container(
-            color: Colors.white,
-            padding: EdgeInsets.all(24),
-            child: Center(
-              child: Column(
-                children: <Widget>[
-                  emailRow,
-                  passwordRow,
-                  loginButtonRow,
-                  registerButtonRow,
-                ],
-              ),
+        child: Container(
+          child: Form(
+            key: formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: inputs() + loginButtons() + registerButtons()
             ),
           ),
         ),
@@ -38,104 +168,83 @@ class _UserLoginState extends State<UserLogin> {//TODO: Criar tipo de letra no i
     );
   }
 
-  final emailRow = Container(
-      child: Column(children: <Widget>[
-        SizedBox( //caixa do username
-          height: 30,
-        ),
-        TextField(
-          autofocus: false,
-          obscureText: false,
-          keyboardType: TextInputType.emailAddress,
-          controller: emailEditingContrller,
-          decoration: InputDecoration(
-              labelText: "Email",
-              labelStyle: TextStyle(
-                color: Colors.black,
-                fontSize: 20,
+
+
+  List<Widget> inputs(){
+    return [
+      TextFormField(
+        decoration: InputDecoration(labelText: 'Email'),
+        validator: (value) => value.isEmpty ? 'Email not valid': null,
+        onSaved: (value) => _email = value,
+      ),
+      TextFormField(
+        decoration: InputDecoration(labelText: 'Password'),
+        obscureText: true,
+        validator: (value) => value.isEmpty ? 'Password not valid': null,
+        onSaved: (value) => _password = value,
+      ),
+    ];
+  }
+
+  List<Widget> loginButtons(){
+    return [
+
+              SizedBox( //caixa da password
+              height: 30,
               ),
-              border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(4)),
-                  borderSide: BorderSide(
-                      width: 1,
-                      color: Colors.green,
-                      style: BorderStyle.solid))),
-        ),
-      ],)
-  );
+              ButtonTheme(
+              //elevation: 4,
+              //color: Colors.green,
+              minWidth: double.infinity,
+                child: MaterialButton(
+                  onPressed: (){
+                    movetologin();
+                    validateandsubmit();
+                    if(_loading == true){
+                      Navigator.push(
+                        currentContext,
+                        MaterialPageRoute(builder: (currentContext) => MainMenu()),
+                      );
+                    }
 
-
-  final passwordRow = Container(
-      child: Column(children: <Widget>[
-        SizedBox( //caixa da password
-          height: 30,
+                  },
+                  textColor: Colors.white,
+                  color: Colors.green,
+                  height: 50,
+                  child: Text("Login"),
         ),
-        TextField(
-          autofocus: false,
-          obscureText: true,
-          keyboardType: TextInputType.text,
-          controller: passwordEditingContrller,
-          decoration: InputDecoration(
-              labelText: "Password",
-              labelStyle: TextStyle(
-                color: Colors.black,
-                fontSize: 20,
-              ),
-              border: OutlineInputBorder(
-                  borderSide: BorderSide(
-                      width: 1,
-                      color: Colors.green,
-                      style: BorderStyle.solid))),
-        ),
-      ],
-      )
-  );
+      ),
+    ];
 
-  final loginButtonRow = Container(
-      child: Column(children: <Widget>[
-        ButtonTheme(
-          //elevation: 4,
-          //color: Colors.green,
-          minWidth: double.infinity,
-          child: MaterialButton(
-            onPressed: () => {
-              //check if user is valid
+  }
+
+  List<Widget> registerButtons(){
+    return [
+      SizedBox( //caixa da password
+        height: 30,
+      ),
+      ButtonTheme(
+        //elevation: 4,
+        //color: Colors.green,
+        minWidth: double.infinity,
+        child: MaterialButton(
+          onPressed: (){
+            movetoregister();
+            validateandsubmit();
+            if(_loading == true){
               Navigator.push(
                 currentContext,
-                MaterialPageRoute(builder: (currentContext) => MainMenu()),
-              )
-            },
-            textColor: Colors.white,
-            color: Colors.green,
-            height: 50,
-            child: Text("Login"),
-          ),
+                MaterialPageRoute(builder: (currentContext) => UserRegister()),
+              );
+            }
+          },
+          textColor: Colors.white,
+          color: Colors.red,
+          height: 50,
+          child: Text("Register"),
         ),
-      ],
-      )
-  );
+      ),
+    ];
 
-  final registerButtonRow = Container(
-      child: Column(children: <Widget>[
-        ButtonTheme(
-          //elevation: 4,
-          //color: Colors.green,
-          minWidth: double.infinity,
-          child: MaterialButton(
-            onPressed: () => {
-              //check if user is valid
-              Navigator.push(
-                currentContext,
-                MaterialPageRoute(builder: (currentContext) => MainMenu()),
-              )
-            },
-            textColor: Colors.white,
-            color: Colors.green,
-            height: 50,
-            child: Text("Register"),
-          ),
-        ),
-      ],
-      )
-  );
+  }
 }
